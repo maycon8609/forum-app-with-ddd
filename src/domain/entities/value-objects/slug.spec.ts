b10eb72b9a578119b783cc4c0bb2f67ba1086dc8
maycon silva <maycon8609@gmail.com>
2024-06-06 19:42:54 -0300

    test: add test to Slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+import { Slug } from './slug'
+
+describe('Value-objects: Slug', () => {
+  it('should be able to create a new slug from text', () => {
+    const slug = Slug.createFromText('Random --mock @any _têxt-')
+
+    expect(slug.value).toEqual('random-mock-any-text')
+  })
+})
