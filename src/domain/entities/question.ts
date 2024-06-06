1798eb11c27df103c4b44cb0efd8c49dc88340d9
maycon silva <maycon8609@gmail.com>
2024-06-06 19:42:27 -0300

    feat: add Slug class

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,12 @@
 import { randomUUID } from "node:crypto"
 
+import { Slug } from "./value-objects/slug"
+
 interface QuestionProps {
   authorId: string
   content: string
   id?: string
+  slug: Slug
   title: string
 }
 
@@ -11,12 +14,14 @@ export class Question {
   public authorId: string
   public content: string
   public id: string
+  public slug: Slug
   public title: string
 
-  constructor({ authorId, content, title, id }: QuestionProps) {
+  constructor({ authorId, content, id, title, slug }: QuestionProps) {
     this.authorId = authorId
     this.content = content
     this.id = id ?? randomUUID()
+    this.slug = slug
     this.title = title
   }
 }
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and normalize it as a slug.
+   * 
+   * Example: "Example title" => "example-title"
+   * 
+   * @param value {string}
+  */
+  static createFromText(value: string) {
+    const slugValue = value
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+      return new Slug(slugValue)
+  }
+}
