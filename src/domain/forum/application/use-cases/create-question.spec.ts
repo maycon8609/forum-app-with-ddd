57a5d4b4bf373a0cd500746d7d36f27680265d2e
maycon silva <maycon8609@gmail.com>
2024-06-11 09:37:29 -0300

    test: update test

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,17 +1,17 @@
 import { CreateQuestionUseCase } from './create-question'
-import { Question } from '../../enterprise/entities/question'
-import { QuestionRepository } from '../repositories/question-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
-const fakeQuestionRepository: QuestionRepository = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  create: async (question: Question) => {},
-}
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase
 
 describe('Use-cases: Create Question', () => {
-  it('should be able create a question', async () => {
-    const createQuestion = new CreateQuestionUseCase(fakeQuestionRepository)
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
+  })
 
-    const { question } = await createQuestion.execute({
+  it('should be able create a question', async () => {
+    const { question } = await sut.execute({
       authorId: '1',
       content: 'New question',
       title: 'Title to question',
@@ -20,5 +20,6 @@ describe('Use-cases: Create Question', () => {
     expect(question.id).toBeTruthy()
     expect(question.content).toEqual('New question')
     expect(question.title).toEqual('Title to question')
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id)
   })
 })
