58da5e93e1303bdb3c60dd4a9dca481e295f1b55
maycon silva <maycon8609@gmail.com>
2024-06-13 15:03:22 -0300

    feat: add EditAnswerUseCase

---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,40 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface EditAnswerUseCaseRequest {
+  authorId: string
+  content: string
+  answerId: string
+}
+
+interface EditAnswerUseCaseResponse {
+  answer: Answer
+}
+
+export class EditAnswerUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    content,
+    answerId,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    answer.content = content
+
+    await this.answerRepository.save(answer)
+
+    return {
+      answer,
+    }
+  }
+}
