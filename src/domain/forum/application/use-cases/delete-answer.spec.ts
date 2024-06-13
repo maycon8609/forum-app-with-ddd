4ab042a21a222415ff88d4b91b05b76b01955cc7
maycon silva <maycon8609@gmail.com>
2024-06-12 22:36:36 -0300

    test: add DeleteAnswerUseCase

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,37 @@
+import { DeleteAnswerUseCase } from './delete-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+
+describe('Use-cases: Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to delete a answer', async () => {
+    const newAnswer = makeAnswer()
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: newAnswer.authorId.toString(),
+      answerId: newAnswer.id.toString(),
+    })
+
+    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a answer from another user', async () => {
+    const newAnswer = makeAnswer()
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    const data = {
+      authorId: 'other-author-id',
+      answerId: newAnswer.id.toString(),
+    }
+
+    expect(() => sut.execute(data)).rejects.toBeInstanceOf(Error)
+  })
+})
