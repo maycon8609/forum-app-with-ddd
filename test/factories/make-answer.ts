ea19b92b662d2f7ff827b85bba1cac5d3bd96bcc
maycon silva <maycon8609@gmail.com>
2024-06-12 22:35:38 -0300

    test: create makeAnswer

---
+++ test/factories/make-answer.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker'
+
+import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export function makeAnswer(
+  override: Partial<AnswerProps> = {},
+  id?: UniqueEntityId,
+) {
+  const answer = Answer.create(
+    {
+      authorId: new UniqueEntityId(),
+      questionId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
+
+  return answer
+}
