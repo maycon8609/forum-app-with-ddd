13d6ace8c4c606f5d1a712b78ff2e854904ac7ab
maycon silva <maycon8609@gmail.com>
2024-06-15 14:06:04 -0300

    feat: add method findManyRecent in the question repository

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionRepository {
@@ -5,5 +6,6 @@ export interface QuestionRepository {
   delete(question: Question): Promise<void>
   findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   save(question: Question): Promise<void>
 }
