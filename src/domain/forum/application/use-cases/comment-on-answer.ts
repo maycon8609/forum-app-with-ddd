bb2e80bf03ffe2d8ae0eb44b02f982b6192ed672
maycon silva <maycon8609@gmail.com>
2024-08-01 18:59:47 -0300

    feat: added useCase to Comment on answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,46 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+export interface CommentOnAnswerUseCaseRequest {
+  authorId: string
+  content: string
+  answerId: string
+}
+
+export interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    content,
+    answerId,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityId(authorId),
+      content,
+      answerId: new UniqueEntityId(answerId),
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
