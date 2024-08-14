47692beeebd615daf0021b6a24ecfd397de9987a
maycon silva <maycon8609@gmail.com>
2024-08-14 17:39:27 -0300

    feat: add QuestionAttachmentsRepository

---
+++ test/factories/make-question-attachment.ts
@@ -0,0 +1,21 @@
+import {
+  QuestionAttachment,
+  QuestionAttachmentProps,
+} from '@/domain/forum/enterprise/entities/question-attachment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export function makeQuestionAttachment(
+  override: Partial<QuestionAttachmentProps> = {},
+  id?: UniqueEntityId,
+) {
+  const questionAttachment = QuestionAttachment.create(
+    {
+      questionId: new UniqueEntityId(),
+      attachmentId: new UniqueEntityId(),
+      ...override,
+    },
+    id,
+  )
+
+  return questionAttachment
+}
