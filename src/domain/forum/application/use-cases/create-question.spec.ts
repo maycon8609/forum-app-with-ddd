3ab31ca08766971b2df7d8ec7d16ecd5459dde72
maycon silva <maycon8609@gmail.com>
2024-08-14 16:58:22 -0300

    test: update test

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
@@ -13,11 +14,17 @@ describe('Use-cases: Create Question', () => {
   it('should be able create a question', async () => {
     const result = await sut.execute({
       authorId: '1',
+      attachmentsIds: ['1', '2'],
       content: 'New question',
       title: 'Title to question',
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+    ])
   })
 })
