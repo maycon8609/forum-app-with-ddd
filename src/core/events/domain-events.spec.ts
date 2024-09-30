7cee7050fbd09d85924f700e9241eebbf223d83c
maycon silva <maycon8609@gmail.com>
2024-09-30 20:08:27 -0300

    test: added domain event

---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,47 @@
+import { vi } from 'vitest'
+
+import { AggregateRoot } from '../entities/aggregate-root'
+import { UniqueEntityId } from '../entities/unique-entity-id'
+import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  private aggregate: CustomAggregate // eslint-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.ocurredAt = new Date()
+    this.aggregate = aggregate
+  }
+
+  public getAggregateId(): UniqueEntityId {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('Core: Domain Events', () => {
+  it('should be able to dispatch and listen to events', () => {
+    const callbackSpy = vi.fn()
+
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+
+    const aggregate = CustomAggregate.create()
+
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callbackSpy).toHaveBeenCalledTimes(1)
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
